8c5ab40b51743d79a7af7f894ffb54cdd98d70df
kimtaekyong <lkss98@gmail.com>
2024-03-27 14:38:52 +0900

    높이 수정

--- js/common.js
+++ js/common.js
@@ -1,9 +1,4 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+document.querySelector("main").style.height = window.innerHeight + "px";
 
 const a = () => {
   console.log("연결 테스트");
