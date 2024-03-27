a6dfb2e17ae1a620e2760d31ed6b682b32ec8aa8
kimtaekyong <lkss98@gmail.com>
2024-03-27 15:15:52 +0900

    높이

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-document.querySelector("main").style.height = window.innerHeight + "px";
+document.querySelector("main").style.height = window.innerHeight - 60 + "px";
 
 const a = () => {
   console.log("연결 테스트");
